c20967a199f63213659261f9d1b85723c2b407ee
Alessandro Roncone <alecive87@gmail.com>
2017-05-05 15:45:40 -0400

    [test] more principled apporach to testing react controller. Not there yet.

--- test/test_react_controller.cpp
+++ test/test_react_controller.cpp
@@ -1,75 +1,99 @@
 #include <gtest/gtest.h>
 
 #include <baxter_collaboration_msgs/GoToPose.h>
-
+#include <robot_utils/utils.h>
 #include "react_controller/baxterChain.h"
 
-/**
- * Imports waypoints from the parameter server
- *
- * @param _nh the NodeHandle
- * @param _wp the waypoints extracted from the parameter server
- *
- * @return true/false if success/failure
- */
-bool importWayPoints(const ros::NodeHandle& _nh, std::vector<geometry_msgs::Pose>& _wp)
+class reactControllerTester
 {
-    _wp.clear();
-    XmlRpc::XmlRpcValue    waypoints;
+private:
+    ros::NodeHandle  nh;
 
-    if(!_nh.getParam("/"+_nh.getNamespace()+"/waypoints", waypoints))
+    ros::Publisher  pub;
+
+    std::vector<geometry_msgs::Pose> _wp;
+
+public:
+    reactControllerTester() : nh("baxter_react_controller")
     {
-        ROS_INFO("No objects' database found in the parameter server. "
-                 "Looked up param is %s", ("/"+_nh.getNamespace()+"/waypoints").c_str());
-        return false;
+        EXPECT_TRUE(importWayPoints(nh, _wp));
+
+        pub = nh.advertise<baxter_collaboration_msgs::GoToPose>(
+             "/baxter_react_controller/right/go_to_pose", 10, true);
     }
 
-    EXPECT_EQ  (waypoints.getType(), XmlRpc::XmlRpcValue::TypeArray);
-    EXPECT_TRUE(waypoints.size());
-
-    for (int i = 0; i < waypoints.size(); ++i)
+    /**
+     * Imports waypoints from the parameter server
+     *
+     * @param _nh the NodeHandle
+     * @param _wp the waypoints extracted from the parameter server
+     *
+     * @return true/false if success/failure
+     */
+    bool importWayPoints(const ros::NodeHandle& _nh, std::vector<geometry_msgs::Pose>& _wp)
     {
-        EXPECT_EQ(waypoints[i].getType(), XmlRpc::XmlRpcValue::TypeArray);
-        EXPECT_EQ(waypoints[i].size()   , 7);
+        _wp.clear();
+        XmlRpc::XmlRpcValue    waypoints;
 
-        geometry_msgs::Pose pose;
-        pose.position.x    = waypoints[i][0];
-        pose.position.y    = waypoints[i][1];
-        pose.position.z    = waypoints[i][2];
-        pose.orientation.x = waypoints[i][3];
-        pose.orientation.y = waypoints[i][4];
-        pose.orientation.z = waypoints[i][5];
-        pose.orientation.w = waypoints[i][6];
+        if(!_nh.getParam("/"+_nh.getNamespace()+"/waypoints", waypoints))
+        {
+            ROS_INFO("No objects' database found in the parameter server. "
+                     "Looked up param is %s", ("/"+_nh.getNamespace()+"/waypoints").c_str());
+            return false;
+        }
 
-        _wp.push_back(pose);
+        EXPECT_EQ  (waypoints.getType(), XmlRpc::XmlRpcValue::TypeArray);
+        EXPECT_TRUE(waypoints.size());
+
+        for (int i = 0; i < waypoints.size(); ++i)
+        {
+            EXPECT_EQ(waypoints[i].getType(), XmlRpc::XmlRpcValue::TypeArray);
+            EXPECT_EQ(waypoints[i].size()   , 7);
+
+            geometry_msgs::Pose pose;
+            pose.position.x    = waypoints[i][0];
+            pose.position.y    = waypoints[i][1];
+            pose.position.z    = waypoints[i][2];
+            pose.orientation.x = waypoints[i][3];
+            pose.orientation.y = waypoints[i][4];
+            pose.orientation.z = waypoints[i][5];
+            pose.orientation.w = waypoints[i][6];
+
+            _wp.push_back(pose);
+        }
+
+        return true;
     }
 
-    return true;
-}
+    bool testWayPoints()
+    {
+        for (size_t i = 0; i < _wp.size(); ++i)
+        {
+            baxter_collaboration_msgs::GoToPose msg;
+            msg.type      = "pose";
+            msg.ctrl_mode =      1;
+
+            msg.position    = _wp[i].position;
+            msg.orientation = _wp[i].orientation;
+
+            ROS_INFO("Testing waypoint %lu: %s", i, toString(_wp[i]).c_str());
+
+            pub.publish(msg);
+            ros::Duration(6.0).sleep();
+        }
+
+        return true;
+    }
+
+    ~reactControllerTester() {};
+};
 
 // Declare a test
 TEST(ReactControllerTest, testWayPoints)
 {
-    ros::NodeHandle  nh("baxter_react_controller");
-    std::vector<geometry_msgs::Pose> _wp;
+    reactControllerTester rct;
 
-    EXPECT_TRUE(importWayPoints(nh, _wp));
-
-    ros::Publisher wp = nh.advertise<baxter_collaboration_msgs::GoToPose>(
-                        "/baxter_react_controller/right/go_to_pose", 10, true);
-
-    for (size_t i = 0; i < _wp.size(); ++i)
-    {
-        baxter_collaboration_msgs::GoToPose msg;
-        msg.type      = "pose";
-        msg.ctrl_mode =      1;
-
-        msg.position    = _wp[i].position;
-        msg.orientation = _wp[i].orientation;
-
-        wp.publish(msg);
-        ros::Duration(10.0).sleep();
-    }
+    EXPECT_TRUE(rct.testWayPoints());
 }
 
 // Run all the tests that were declared with TEST()
