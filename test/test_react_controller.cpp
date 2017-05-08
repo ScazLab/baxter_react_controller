76e2f59efe204ee8a002af49e02c11abdfdd25aa
Alessandro Roncone <alecive87@gmail.com>
2017-05-08 15:32:01 -0400

    [test] Hard coded enable and untuck script into test, plus added some more utilities. Now the test runs fully within one roslaunch file.

--- test/test_react_controller.cpp
+++ test/test_react_controller.cpp
@@ -2,10 +2,12 @@
 
 #include <baxter_collaboration_msgs/GoToPose.h>
 #include <baxter_collaboration_msgs/ArmState.h>
+#include <baxter_core_msgs/AssemblyState.h>
 #include <robot_utils/utils.h>
 #include "react_controller/baxterChain.h"
 
-#include <memory>
+#include   <memory>
+#include <stdlib.h>
 
 class reactControllerTester
 {
@@ -14,10 +16,12 @@ private:
 
     std::string    limb;
 
-    ros::Publisher  pub;
-    ros::Subscriber sub;  // Subscriber for the arm state
+    ros::Publisher     pub;
+    ros::Subscriber as_sub;  // Subscriber for the arm   state
+    ros::Subscriber gz_sub;  // Subscriber for the robot state
 
-    std::shared_ptr<baxter_collaboration_msgs::ArmState> arm_state_ptr;
+    std::shared_ptr<baxter_collaboration_msgs::ArmState> as_state_ptr;
+    std::shared_ptr< baxter_core_msgs::AssemblyState>    gz_state_ptr;
 
     std::vector<geometry_msgs::Pose> _wp;
 
@@ -71,22 +75,89 @@ private:
      */
     void armStateCb(const baxter_collaboration_msgs::ArmState& msg)
     {
-        *arm_state_ptr = msg;
+        as_state_ptr.reset(new baxter_collaboration_msgs::ArmState(msg));
+    };
+
+    void robotStateCb(const baxter_core_msgs::AssemblyState& msg)
+    {
+        gz_state_ptr.reset(new baxter_core_msgs::AssemblyState(msg));
     };
 
 public:
     reactControllerTester(std::string _limb) : nh("test_react_controller"), limb(_limb),
-                                               arm_state_ptr(new baxter_collaboration_msgs::ArmState)
+                                               as_state_ptr(new baxter_collaboration_msgs::ArmState),
+                                               gz_state_ptr(nullptr)
     {
         EXPECT_TRUE(importWayPoints(nh, _wp));
 
         pub = nh.advertise<baxter_collaboration_msgs::GoToPose>(
-             "/baxter_react_controller/" + limb + "/go_to_pose", 10, true);
+              "/baxter_react_controller/" + limb + "/go_to_pose", SUBSCRIBER_BUFFER, true);
 
-        sub = nh.subscribe("/baxter_react_controller/" + limb + "/state", SUBSCRIBER_BUFFER,
+        as_sub = nh.subscribe("/baxter_react_controller/" + limb + "/state", SUBSCRIBER_BUFFER,
                                                   &reactControllerTester::armStateCb, this);
+
+        gz_sub = nh.subscribe("/robot/state", SUBSCRIBER_BUFFER,
+                              &reactControllerTester::robotStateCb, this);
     };
 
+    /**
+     * This is very dirty because of how roslaunch and rostests are handled, but it does the job.
+     * Wait for robot/state to be published, so that we know that the robot is up and running
+     * on the Gazebo server. After this, the robot still needs to be enabled and untucked.
+     *
+     * @param _wait_time time to wait for the robot to be up in Gazebo
+     * @return true/false if the robot was up within the given amount of time
+     */
+    bool waitBaxterGazeboReady(double _wait_time = 60.0)
+    {
+        ros::Time _init = ros::Time::now();
+
+        ros::Rate r(100);
+        while (ros::ok())
+        {
+            if (bool(gz_state_ptr))     { return true; }
+
+            if ((ros::Time::now()-_init).toSec() > _wait_time)
+            {
+                ROS_WARN("No robot state in %gs!", _wait_time);
+                return false;
+            }
+
+            ros::spinOnce();
+            r.sleep();
+        }
+
+        return false;
+    }
+
+    /*
+     * Waits for the robot to be enabled
+     *
+     * @param _wait_time time to wait for the robot to be up in Gazebo
+     * @return true/false if the robot was enabled within the given amount of time
+     */
+    bool waitRobotEnabled(double _wait_time = 4.0)
+    {
+        clearRobotState();
+        ros::Time _init = ros::Time::now();
+
+        ros::Rate r(100);
+        while (ros::ok())
+        {
+            if (isEnabled())     { return true; }
+
+            if ((ros::Time::now()-_init).toSec() > _wait_time)
+            {
+                ROS_WARN("Robot not enabled in %gs!", _wait_time);
+                return false;
+            }
+
+            ros::spinOnce();
+            r.sleep();
+        }
+
+        return false;
+    }
 
     /*
      * Waits for states to be received from the subscriber.
@@ -101,16 +172,16 @@ public:
         ros::Rate r(100);
         while (ros::ok())
         {
-            if (std::string(_as) == arm_state_ptr->state)     { return true; }
-
-            ros::spinOnce();
-            r.sleep();
+            if (std::string(_as) == as_state_ptr->state)     { return true; }
 
             if ((ros::Time::now()-_init).toSec() > _wait_time)
             {
                 ROS_WARN("No reactController state %s in %gs!", std::string(_as).c_str(), _wait_time);
                 return false;
             }
+
+            ros::spinOnce();
+            r.sleep();
         }
 
         return false;
@@ -137,8 +208,21 @@ public:
             pub.publish(msg);
 
             ASSERT_TRUE(waitReactCtrlState(State(CTRL_RUNNING),  4.0));
-            ASSERT_TRUE(waitReactCtrlState(State(   CTRL_DONE), 40.0));
+            ASSERT_TRUE(waitReactCtrlState(State(   CTRL_DONE), 60.0));
         }
+    };
+
+    bool isEnabled()
+    {
+        if (not bool(gz_state_ptr))         { return false; }
+
+        return gz_state_ptr -> enabled;
+    };
+
+    bool clearRobotState()
+    {
+        gz_state_ptr = nullptr;
+        return true;
     };
 
     ~reactControllerTester() {};
@@ -149,11 +233,24 @@ TEST(ReactControllerTest, testWayPoints)
 {
     reactControllerTester rct("right");
 
+    // DO NOT remove this sleep
+    ros::Duration(0.1).sleep();
+
+    ROS_INFO("Waiting for robot to be up..");
+    ASSERT_TRUE(rct.waitBaxterGazeboReady());
+
+    ROS_INFO("Enabling robot..");
+    ASSERT_FALSE(system("rosrun baxter_tools enable_robot.py -e"));
+    ASSERT_TRUE(rct.waitRobotEnabled());
+
+    ROS_INFO("Untucking robot..");
+    ASSERT_FALSE(system("rosrun baxter_tools tuck_arms.py -u"));
+
     ROS_INFO("Waiting for robot to be ready..");
     // CTRL_DONE is there to test the tester, but it should be removed.
     bool test_waypoints = rct.waitReactCtrlState(State(CTRL_DONE), 10.0) ||
-                          rct.waitReactCtrlState(State(START), 10.0);
-    EXPECT_TRUE(test_waypoints);
+                          rct.waitReactCtrlState(State(    START), 10.0);
+    ASSERT_TRUE(test_waypoints);
 
     ROS_INFO("Done. Testing waypoints..");
     rct.testWayPoints();
